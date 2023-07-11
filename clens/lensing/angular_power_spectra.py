fixed bug in scaling relation

clens/lensing/angular_power_spectra.py
@@ -91,7 +91,7 @@ class AngularPowerSpectra(object):
         self.ell_kappa = self.ell
         self.C_ell_kappa = C_ell_sum
 
-        n_src_sr = self.su.n_src_arcmin/cn.arcmin_to_radian**2
+        n_src_sr = self.su.n_src_arcmin/cn.arcmin_to_radian**2 # TODO: I should integrate above zh+0.01??
         self.shape_noise = self.su.sigma_gamma**2/n_src_sr
         #print('n_src_sr', n_src_sr)
 
@@ -139,10 +139,12 @@ class AngularPowerSpectra(object):
         self.ell_Sigma = self.ell
         self.C_ell_Sigma = C_ell_sum
 
-        n_src_sr = self.su.n_src_arcmin/cn.arcmin_to_radian**2
+        print('fsrc_behind_zh =', zh, self.lk.fsrc_behind_zh(zh))
+        n_src_sr = self.su.n_src_arcmin * self.lk.fsrc_behind_zh(zh)/cn.arcmin_to_radian**2
         mean_Sigma_crit = self.lk.mean_Sigma_crit(zh=zh)
-        #print('mean_Sigma_crit', mean_Sigma_crit)
+        print('mean_Sigma_crit %g'%mean_Sigma_crit)
         self.shape_noise_for_Sigma = self.su.sigma_gamma**2/n_src_sr * mean_Sigma_crit**2 
+        print('self.shape_noise_for_Sigma', self.shape_noise_for_Sigma)
 
     ###### halo-halo ######
     def calc_C_ell_h(self, zh_min, zh_max, lambda_min, lambda_max):
@@ -155,13 +157,16 @@ class AngularPowerSpectra(object):
         zh_max_list = zh_bins[1:]
 
         # get the halo number density and bias
-        survey_area_sq_deg = 41253./48.# exact value doesn't matter
+        survey_area_sq_deg = 1437.#41253.#/48.# exact value doesn't matter
         cc = ClusterCounts(cosmo_parameters=self.co, scaling_relation=self.sr)
         cc.calc_counts(zmin=zh_min, zmax=zh_max, lambda_min=lambda_min, lambda_max=lambda_max, survey_area_sq_deg=survey_area_sq_deg)
         #n_h_Mpc3 = cc.cluster_number_density
         area_sr = 4.*np.pi*survey_area_sq_deg/41253.
-        print('cc.counts', cc.counts)
+
+        print('lambda', lambda_min, lambda_max, 'cc.counts', cc.counts)
         n_h_sr = cc.counts/area_sr
+        #print('n_h_sr', n_h_sr)
+        #exit()
         b = cc.cluster_mean_bias
         print('bias', b)
         #exit()

clens/lensing/cov_DeltaSigma.py
@@ -6,7 +6,7 @@ from scipy.interpolate import interp1d
 from astropy.cosmology import FlatLambdaCDM
 
 import clens.util.constants as cn
-from clens.util.parameters import CosmoParameters#, NuisanceParameters
+from clens.util.parameters import CosmoParameters
 from clens.util.scaling_relation import FiducialScalingRelation, Costanzi21ScalingRelation, Murata18ScalingRelation
 from clens.util.survey import Survey
 
@@ -198,8 +198,8 @@ class CovDeltaSigma(object):
 def demo_cov(plotting=False):
     co = CosmoParameters()
     #sr = FiducialScalingRelation()
-    sr = Costanzi21ScalingRelation()
-    #sr = Murata18ScalingRelation()
+    #sr = Costanzi21ScalingRelation()
+    sr = Murata18ScalingRelation()
     su = Survey(zs_min=0.56, zs_max=0.65, top_hat=True, n_src_arcmin=10, sigma_gamma=0.3)
     fsky = 5000. / 41253.
     cds = CovDeltaSigma(co=co, su=su, sr=sr, fsky=fsky)

clens/util/cluster_counts.py
@@ -12,9 +12,8 @@ from clens.ying.density import Density
 from clens.ying.halostat import HaloStat
 
 from clens.util import constants as cn
-from clens.util.parameters import CosmoParameters#, NuisanceParameters, NuisanceParametersPiecewise
-from clens.util.scaling_relation import RichnessSelection, FiducialScalingRelation, Costanzi21ScalingRelation
-
+from clens.util.parameters import CosmoParameters
+from clens.util.scaling_relation import RichnessSelection, FiducialScalingRelation, Costanzi21ScalingRelation, To21ScalingRelation
 
 class ClusterCounts(object):
     def __init__(self, cosmo_parameters, scaling_relation):
@@ -25,9 +24,9 @@ class ClusterCounts(object):
     def calc_counts(self, zmin, zmax, lambda_min, lambda_max, survey_area_sq_deg):
         fsky = survey_area_sq_deg/41253.
         cosmo = FlatLambdaCDM(H0=self.cp.h*100, Om0=self.cp.OmegaM)
-        vol = (cosmo.comoving_volume(zmax).value - cosmo.comoving_volume(zmin).value)*fsky
-        #print('vol', vol*1e-9)
-        
+        vol = fsky * 4. * np.pi/3. * (cosmo.comoving_distance(zmax).value**3 - cosmo.comoving_distance(zmin).value**3)
+        print('vol', vol)
+
         z = 0.5*(zmin+zmax)
         # using ying's mass function for now
         self.cosmo = CosmoParams(omega_M_0=self.cp.OmegaM, omega_b_0=self.cp.OmegaB, omega_lambda_0=self.cp.OmegaDE, h=self.cp.h, sigma_8=self.cp.sigma8, n=self.cp.ns, tau=self.cp.tau)
@@ -70,10 +69,12 @@ class ClusterCounts(object):
         return counts, sv, mean_bias # #lnM_mean
 
 if __name__ == "__main__":
-    cosmo_parameters = CosmoParameters()
-    scaling_relation = FiducialScalingRelation()
+    #cosmo_parameters = CosmoParameters()
+    cosmo_parameters = CosmoParameters(h=0.7, OmegaDE=0.724, OmegaM=0.276, sigma8=0.802)
+    #scaling_relation = FiducialScalingRelation()
     #scaling_relation = Costanzi21ScalingRelation()
+    scaling_relation = To21ScalingRelation()
     cmm = ClusterCounts(cosmo_parameters=cosmo_parameters, scaling_relation=scaling_relation)
-    cc = cmm.calc_counts(zmin=0.2, zmax=0.35, lambda_min=20, lambda_max=30, survey_area_sq_deg=1000)
+    cc = cmm.calc_counts(zmin=0.2, zmax=0.35, lambda_min=20, lambda_max=30, survey_area_sq_deg=1437)
     print(cc)
 

clens/util/scaling_relation.py
@@ -50,6 +50,22 @@ class Costanzi21ScalingRelation(object):
         sigma_sqr = self.Dlam**2 + (lam - 1)/lam**2
         return np.sqrt(sigma_sqr)
 
+class To21ScalingRelation(object):
+    def __init__(self, lnlam0=4.26, A_lnlam=0.943, B_lnlam=0.15, sig_lnlam=0.299):
+        self.lnlam0 = lnlam0
+        self.A_lnlam = A_lnlam
+        self.B_lnlam = B_lnlam
+        self.sig_lnlam = sig_lnlam
+
+    def lnlambda_lnM(self, lnM, z):
+        lnMpivot_Msun = np.log(5e14/0.7) # Note: should this be 3e14? C21 paper
+        return self.lnlam0 + self.A_lnlam * (lnM - lnMpivot_Msun) + self.B_lnlam * np.log((1+z)/1.45)
+
+    def scatter(self, lnM, z):
+        lam = np.exp(self.lnlambda_lnM(lnM, z))
+        sigma_sqr = self.sig_lnlam**2 + (lam - 1)/lam**2
+        return np.sqrt(sigma_sqr)
+
 class Murata18ScalingRelation(object):
     def __init__(self, A_M18=3.207, B_M18=0.993, sigma0_M18=0.456, q_M18=-0.169):
         self.A_M18 = A_M18
@@ -69,10 +85,10 @@ class Murata18ScalingRelation(object):
         sigma[sigma <= 1e-8] = 1e-8  # avoid negative values
         return sigma
 
-def plot_lambda_M(scaling_relation):
+def plot_lambda_M(scaling_relation, label=None):
     lgM_arr = np.arange(13.5,15.1,0.01)
     z = 1
-    plt.plot(10**lgM_arr, np.exp(scaling_relation.lnlambda_lnM(lgM_arr*np.log(10.), z)), label='original')
+    plt.plot(10**lgM_arr, np.exp(scaling_relation.lnlambda_lnM(lgM_arr*np.log(10.), z)), label=label)
     plt.xscale('log')
     plt.yscale('log')
     plt.xlabel(r'$\rm M_{200m}$')